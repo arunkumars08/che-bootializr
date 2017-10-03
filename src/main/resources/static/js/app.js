e8c72230cd7d2352192e0adcd2607fb4d7abe9bf
Kamesh Sampath <kameshsampath@hotmail.com>
2017-10-03 09:31:29 +0530

    added tags and dependencies selection
    WIP:
     - show collapsible deps list
     - Git workflow optimization
     - error handling
     - che command to add fmp on loading project on to workspace

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,3 +1,78 @@
 $(function () {
 
+    var addTag = function (id, name) {
+        if ($("#depsTags div[data-id='" + id + "']").length == 0) {
+            $("#depsTags").append("<div class='tag' data-id='" + id + "'>" + name +
+                "<button type='button' class='close' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>");
+        }
+    };
+
+    var removeTag = function (id) {
+        $("#depsTags div[data-id='" + id + "']").remove();
+    };
+
+    var isTagExists = function (id) {
+        return $("#depsTags div[data-id='" + id + "']") === undefined;
+    };
+
+    var depsSearcher = function (engine, bootVersion) {
+
+        $.getJSON({
+            url: '/ui/dependencies.json?version=' + bootVersion
+        }).done(function (data) {
+            engine.clear();
+            $.each(data.dependencies, function (i, item) {
+                if (item.weight === undefined) {
+                    item.weight = 0;
+                }
+            });
+            engine.add(data.dependencies);
+        })
+    };
+
+    var bDeps = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.nonword('group', 'name', 'description', 'keywords'),
+        queryTokenizer: Bloodhound.tokenizers.nonword,
+        identify: function (item) {
+            return item.id;
+        },
+        sorter: function (left, right) {
+            return left.weight - right.weight;
+        },
+        cache: false,
+        limit: 5
+    });
+
+    depsSearcher(bDeps, $('#bootVersion').val());
+
+    $("#bootVersion").on("change", function (e) {
+        depsSearcher(bDeps, this.value);
+    });
+
+    $('#depsList').typeahead({
+        minLength: 2,
+        autoSelect: true,
+    }, {
+        name: 'bDeps',
+        display: 'name',
+        source: bDeps,
+        templates: {
+            suggestion: function (dep) {
+                return "<div><strong>" + dep.name + "</strong>&nbsp;&nbsp;<small>" + dep.description + "</small></div>"
+            }
+        }
+    });
+
+
+    $('#depsList').bind('typeahead:select', function (event, dep) {
+        if (!isTagExists(dep.id)) {
+            addTag(dep.id, dep.name);
+        }
+    });
+
+    $('#depsTags').on('click', "button", function () {
+        var id = $(this).parent().attr('data-id');
+        removeTag(id);
+    })
+
 });
